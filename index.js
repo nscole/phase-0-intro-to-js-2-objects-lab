8234c166acda3860d82056878c16a519e53cd3bd
Naomi Cole <naomisarahcole@gmail.com>
2022-05-31 14:35:09 +1000

    3 passin g 4 failing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {
+ name: "Naomi Cole",
+ streetAddress: "32 Wallaby Way"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, {[key]: value});
+};
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = 'Sam';
+    employee.streetAddress = '12 Broadway';
+    return employee;
+};
+
+fuction deleteFromEmployeeByKey(employee, key) {
+
+};
+
