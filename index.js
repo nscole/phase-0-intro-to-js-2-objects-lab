1549d060fa51be1b1366d40f659930e9bd2b9e81
Naomi Cole <naomisarahcole@gmail.com>
2022-06-07 13:45:05 +1000

    3 passing Phase-1

--- index.js
+++ index.js
@@ -1,74 +1,22 @@
 
 let employee = {
     name: "Jennifer Aniston",
-    streetAddress: "11 Broadway",
+    streetAddress: "32 Wallaby Way",
 };
 
 function updateEmployeeWithKeyAndValue(employee, key, value){
    return Object.assign({}, employee, {[key]: value});
 }
 
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = 'Sam';
+    employee.streetAddress = '12 Broadway';
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    return Object.assign({}, delete employee.key);
+}
 
 
 
-
-
-//       it('returns an employee with the original key value pairs and the new key value pair', function () {
-//         expect(updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway')).to.eql({
-//           name: 'Sam',
-//           streetAddress: '11 Broadway'
-//         });
-//       });
-  
-//       it('it does not modify the original employee, but rather returns a clone with the new data', function () {
-//         updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
-  
-//         expect(employee['streetAddress']).to.equal(undefined);
-//       });
-//     });
-  
-//     describe('destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)', function () {
-//       it('updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee', function () {
-//         expect(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway')).to.eql({
-//           name: 'Sam',
-//           streetAddress: '12 Broadway'
-//         });
-  
-//         expect(employee).to.eql({
-//           name: 'Sam',
-//           streetAddress: '12 Broadway'
-//         });
-//       });
-//     });
-  
-//     describe('deleteFromEmployeeByKey(employee, key)', function () {
-//       it('deletes `key` from a clone of employee and returns the new employee (it is non-destructive)', function () {
-//         let newEmployee = deleteFromEmployeeByKey(employee, 'name');
-  
-//         expect(newEmployee['name']).to.equal(undefined);
-//         expect(typeof newEmployee).to.equal('object');
-//       });
-  
-//       it('does not modify the original employee (it is non-destructive)', function () {
-//         deleteFromEmployeeByKey(employee, 'name');
-  
-//         expect(employee['name']).to.equal('Sam');
-//       });
-//     });
-  
-//     describe('destructivelyDeleteFromEmployeeByKey(employee, key)', function () {
-//       it('returns employee without the delete key/value pair', function () {
-//         let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
-  
-//         expect(newEmployee['name']).to.equal(undefined);
-//       });
-  
-//       it('modifies the original employee', function () {
-//         let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
-  
-//         expect(employee['name']).to.equal(undefined);
-//         expect(employee).to.equal(newEmployee);
-//       });
-//     });
-//   });
-  
